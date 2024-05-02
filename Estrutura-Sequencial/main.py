2072088f428ad7538bf60b2a3a76dcec9cd617a6
Amanda <amanda.admoliveira1@gmail.com>
2024-05-02 01:01:11 -0300

    Criação do menu, estrutura condicional e de looping

---
+++ Estrutura-Sequencial/main.py
@@ -0,0 +1,51 @@
+# Primeiros programas feitos para fixação da linguagem
+import os
+
+while True:
+    os.system('cls')
+    menu= input('''
+Bem vindo(a) ao seu calculador de coisas específicas,
+Escolha uma das opções abaixo:
+[1] Cálculo de Salário
+[2] Calcular número ²(ao quadrado) e √(raiz quadrada)
+[3] Cálculo de Ração
+[4] Calcular média ponderada
+[5] Cáculo de convites que devem ser vendidos
+[6] Cálculo de desconto de salário
+[7] Cálculo para iluminação
+[8] Sair         
+''')
+
+    if menu not in ["1","2","3","4","5","6""7","8"]:
+        input("PRESSIONE ENTER PARA VOLTAR AO MENU")
+    elif menu == "1":
+        
+
+        input("PRESSIONE ENTER PARA VOLTAR AO MENU")
+    elif menu == "2":
+
+
+        input("PRESSIONE ENTER PARA VOLTAR AO MENU")
+    elif menu == "3":
+        
+
+        input("PRESSIONE ENTER PARA VOLTAR AO MENU")
+    elif menu == "4":
+        
+
+        input("PRESSIONE ENTER PARA VOLTAR AO MENU")
+    elif menu == "5":
+        
+        
+        input("PRESSIONE ENTER PARA VOLTAR AO MENU")
+    elif menu == "6":
+        
+
+        input("PRESSIONE ENTER PARA VOLTAR AO MENU")
+    elif menu == "7":
+        
+        
+        input("PRESSIONE ENTER PARA VOLTAR AO MENU")
+    else:
+        print("Tchau, Até a próxima!")
+        break
